4ed182a8634d49ff781c7266d745898fc2b43f6b
Owner <Owner@DESKTOP-AR0O5IR>
2026-03-21 18:36:06 -0400

    probado proyectoRencar

---
+++ src/main/java/com/mycompany/proyectorencar/ProyectoRencar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectorencar;
+
+/**
+ *
+ * @author Owner
+ */
+public class ProyectoRencar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
